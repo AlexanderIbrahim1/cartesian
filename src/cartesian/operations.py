4189132db0b9867a2538b10701a5f85800cb9d60
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2022-09-13 12:04:34 -0400

    Add linear combination

---
+++ src/cartesian/operations.py
@@ -0,0 +1,20 @@
+"""
+This module contains functions to perform common operations on CartesianND points,
+such as:
+    - taking linear combinations of several points
+    - ...
+"""
+from array import array
+from typing import Sequence
+
+from cartesian import CartesianND
+
+
+def linear_combination(points: Sequence[CartesianND], coeffs: Sequence[CartesianND]) -> Sequence[CartesianND]:
+    assert len(points) == len(coeffs) > 0
+    
+    sum_point = coeffs[0] * points[0]
+    for (point, coeff) in zip(points[1:], coeffs[1:]):
+        sum_point = sum_point + coeff * point
+    
+    return sum_point
